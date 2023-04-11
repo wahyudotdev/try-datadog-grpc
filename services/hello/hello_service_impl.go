8bbceb1aa7d296b784c3746cb84c3a3272333557
wahyudotdev <wahyudotdev@gmail.com>
2023-04-12 06:34:05 +0700

    first commit

--- services/hello/hello_service_impl.go
+++ services/hello/hello_service_impl.go
@@ -2,14 +2,12 @@ package hello
 
 import (
 	context "context"
-	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
 type ServiceImpl struct {
 }
 
 func (h ServiceImpl) Hello(ctx context.Context, request *HelloRequest) (*HelloResponse, error) {
-	tracer.SpanFromContext(ctx)
 	return &HelloResponse{
 		Message: "Hello",
 	}, nil
